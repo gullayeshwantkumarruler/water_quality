0599cfdc5c3c96abb58899c347940fa0a66bbfee
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2023-07-24 10:44:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,39 +4,51 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.preprocessing import LabelEncoder
 import pickle
 
 # Load the trained model from a pickle file
-model = pickle.load(open('rf_final_model_maternity.pkl', 'rb'))
+model = pickle.load(open('rf_model_water_quality.pkl', 'rb'))
 
 # Streamlit app
-st.title("Maternity Health Prediction")
+st.title("Water Quality Predictor")
+st.sidebar.title("Input Features")
+st.sidebar.markdown("Enter the values for the following features:")
 st.image("image_indoor.jpg", use_column_width=True)
 
 # Input features
-age = st.number_input("Age")
-systolic_bp = st.number_input("Systolic Blood Pressure")
-diastolic_bp = st.number_input("Diastolic Blood Pressure")
-bs = st.number_input("Blood Sugar Level")
-body_temp = st.number_input("Body Temperature")
-heart_rate = st.number_input("Heart Rate")
+station_name = st.text_input("Station Name")
+primary_basin = st.text_input("Primary Basin")
+depth = st.number_input("Depth")
+ph = st.number_input("PH")
+
 
 # Predict button
 if st.button("Predict"):
     # Create a DataFrame from the user inputs
     input_data = pd.DataFrame({
-        'Age': [age],
-        'SystolicBP': [systolic_bp],
-        'DiastolicBP': [diastolic_bp],
-        'BS': [bs],
-        'BodyTemp': [body_temp],
-        'HeartRate': [heart_rate]
+        'Station_Name': [station_name],
+        'Primary_Basin': [primary_basin],
+        'Depth': [depth],
+        'PH': [ph]
     })
 
-    # Make the prediction using the loaded model
-    prediction = model.predict(input_data)[0]
+# Load the label encoder for 'Station_Name' and 'Primary_Basin' from a saved file
+label_encoder = LabelEncoder()
+# label_encoder.classes_ = np.load('label_encoder_classes.npy', allow_pickle=True)
 
-    # Display the predicted risk level
-    st.header("Risk Level Prediction:")
-    st.write(prediction)
+# Perform label encoding on 'Station_Name' and 'Primary_Basin' columns
+input_data['Station_Name'] = label_encoder.fit_transform(input_data['Station_Name'])
+input_data['Primary_Basin'] = label_encoder.fit_transform(input_data['Primary_Basin'])
+
+# Make the prediction using the loaded model
+prediction = model.predict(input_data)[0]
+
+# Display the predicted risk level
+st.header("Prediction")
+st.write(prediction)
+# Display the prediction
+if (prediction[0] == 1) or (prediction[0]==0:
+    st.success("The water quality is predicted to be good.")
+else:
+    st.error("The water quality is predicted to be poor.")
